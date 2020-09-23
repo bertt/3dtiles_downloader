f0512581977194754bde4c5b35eda73716ba3e10
bertt <btemme@hotmail.com>
2020-09-23 10:28:18 +0200

    initial

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # 3dtiles_exporter
+
+Console tool for downloading 3d Tiles from remote server. 
+
+Input: tileset.json
+
+Roadmap:
+
+. Add hierarchical tileset.json support
---
+++ src/Program.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.IO;
+using System.Net.Http;
+using System.Threading.Tasks;
+using threedtiles_exporter;
+
+namespace src
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var tilesetjson = File.ReadAllText("tileset.json");
+            var json = JsonConvert.DeserializeObject<TileSetJson>(tilesetjson);
+            var httpClient = new HttpClient();
+            foreach (var tile in json.root.children)
+            {
+                var tile_url = tile.content.uri;
+                var res = await httpClient.GetAsync($"http://myserver/{tile_url}");
+                var bytes = await res.Content.ReadAsByteArrayAsync();
+                File.WriteAllBytes(tile_url, bytes);
+                Console.Write(".");
+            }
+
+        }
+    }
+}
